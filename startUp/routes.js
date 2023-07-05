afc609a7705db6b74d7ec1f7434f3b0e4606b8d3
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-07-05 17:00:48 +0300

    update routes for subscription

--- startUp/routes.js
+++ startUp/routes.js
@@ -3,6 +3,8 @@ const users = require("../routes/users");
 const auth = require("../routes/auth");
 const customers = require("../routes/customer");
 const genres = require("../routes/genres");
+const movies = require("../routes/movie");
+const subscription = require("../routes/subscription");
 const error = require("../middleware/error");
 
 module.exports = function (app) {
@@ -10,6 +12,8 @@ module.exports = function (app) {
   app.use("/api/users", users);
   app.use("/api/customers", customers);
   app.use("/api/genres", genres);
+  app.use("/api/movies", movies);
+  app.use("/api/subscription", subscription);
   app.use("/api/auth", auth);
   app.use(error);
 };
