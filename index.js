532b7d84486015ea9f28623f6edbde77b30b8fd3
ABDI TIRUNEH <abdi.tirunehh@gmail.com>
2023-07-05 17:03:25 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const winston = require("winston");
 const express = require("express");
 const config = require("config");
 const app = express();
 
+require("./startUp/logging")();
 require("./startUp/routes")(app);
 require("./startUp/db")();
+require("./startUp/config")();
+require("./startUp/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () => {
